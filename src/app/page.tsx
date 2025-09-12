53980d80a043cd6ef185f7f91f3b43852198bc8d
khavani95 <khavani@live.com>
2025-09-12 08:31:17 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Splash() {
  return (
   <div className="flex items-center justify-center h-screen bg-white">
     <img
-      src="/Amud.gif"
+      src="/AMUD3.svg"
       alt="لوگو آمود"
       className="w-[550px] h-[550px]" // لوگو سه برابر
     />
