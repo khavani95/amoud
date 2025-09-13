b7878ef5830b475e80256d4958a8f157bdf4630d
khavani95 <khavani@live.com>
2025-09-13 16:01:29 +0430

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    eslint: {
-    ignoreDuringBuilds: true, // ⬅️ این خط بیلد رو ادامه میده حتی اگه ارور ESLint باشه
-  },
 };
 
 export default nextConfig;
